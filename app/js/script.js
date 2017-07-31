ddfa0364643ec140ff17c91bcc0768f0b13cdfc7
unknown <JLEGREC@JULIO.domho.local>
2017-07-31 13:03:25 +0400

    Made temperatur round to the nearest integer

--- app/js/script.js
+++ app/js/script.js
@@ -8,16 +8,14 @@ var myRequest = new XMLHttpRequest();
 
 navigator.geolocation.getCurrentPosition(function(position) {
    currentLatitude = position.coords.latitude;
-   currentLongitude = position.coords.longitude;
+	 currentLongitude = position.coords.longitude;
    makeRequest(); 
 });
-
-// [43.3,5.38]
 
 function makeRequest() {
 	var method = "GET",
 			url = `http://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&APPID=5e54985f3712fd90887ced9539d73345`;
-			// url = `http://api.openweathermap.org/data/2.5/weather?lat=43&lon=5&APPID=5e54985f3712fd90887ced9539d73345`;
+			// url = `http://api.openweathermap.org/data/2.5/weather?lat=-20.16&lon=57.5&APPID=5e54985f3712fd90887ced9539d73345`;
 	myRequest.open(method, url, true);
 	myRequest.onreadystatechange = displayInfo;
 	myRequest.send();
@@ -28,7 +26,7 @@ function displayInfo() {
 		var myResponseText = JSON.parse(myRequest.responseText);
 		console.log(myResponseText);
 		KelvinTemp = myResponseText.main.temp;
-		document.querySelector('.temp').innerHTML = `${KelvinTemp - 273.15}&deg;C`;
+		document.querySelector('.temp').innerHTML = `${Math.round(KelvinTemp - 273.15)}&deg;C`;
 		document.querySelector('.location').innerHTML = `Location: ${myResponseText.name}, ${myResponseText.sys.country}`;
 		document.querySelector('.weather-description').innerHTML = myResponseText.weather[0].description;
 
